cf722a59558129b37151fe28ad6497f36e464c2d
wittch <76587976+wittch@users.noreply.github.com>
2022-09-05 16:30:08 +0900

    Segtree class init added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
 
+int init(vector<int>& v, vector<int>& tree, int node, int start, int end)
+{
+	if (start == end)
+	{
+		return tree[node] = v[start];
+	}
+
+
+	return tree[node] = init(v, tree, node * 2, start, (start + end) / 2) + init(v, tree, node * 2 + 1, (start + end) / 2 + 1, end);
+}
 int main()
 {
 	cout << "Times fly" << '\n';
