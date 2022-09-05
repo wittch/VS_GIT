140e478756a6d61dcc96572332f9aca4f8d8e427
wittch <76587976+wittch@users.noreply.github.com>
2022-09-05 16:40:23 +0900

    Add segtree class 'sum'

--- main.cpp
+++ main.cpp
@@ -22,6 +22,14 @@ void update(vector<int>& tree, int node, int start, int end, int index, int diff
 		update(tree, node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
 	}
 }
+int sum(vector<int>& tree, int node, int start, int end, int left, int right)
+{
+	if (left > start || end < right) return 0;
+	if (start <= left && right <= end) return tree[node];
+	
+	return sum(tree, node * 2, start, (start + end) / 2, left, right) + sum(tree, node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+	
+}
 int main()
 {
 	cout << "Times fly" << '\n';
